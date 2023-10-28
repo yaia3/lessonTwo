c0b9cfa3abec81b6a1489dfb86264e89e9a7595d
yaia3 <a.novgorodov@indriver.com>
2023-10-28 22:34:49 +0900

    Initial commit

---
+++ src/test/java/com/demo/FirstTest.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstTest {
+
+    @Test
+    void shouldOpen() {
+        String date = "28.11.2020";
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate localDate = LocalDate.parse(date, formatter);
+
+        assertEquals(DayOfWeek.SATURDAY, localDate.getDayOfWeek());
+    }
+}
